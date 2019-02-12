e525d61de38f863d242c18655e3560a31de48dfd
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-02-12 09:13:01 +0200

    Fix env name

--- do-backup.sh
+++ do-backup.sh
@@ -5,8 +5,8 @@ DUMP_FILE=${DUMP_DIRECTORY}/${BACKUP_PREFIX}-`date +%d%m%yT%H%M.tar.gz`
 
 echo "Dumping database to ${DUMP_FILE}..."
 
-rabbitmqctl --erlang-cookie $RABBITMQ_ERLANG_COOKIE -n rabbit@$DB_HOST stop_app
+rabbitmqctl --erlang-cookie $RABBITMQ_ERLANG_COOKIE -n rabbit@$MQ_HOST stop_app
 tar -C $DATA_DIRECTORY -czf $DUMP_FILE .
-rabbitmqctl --erlang-cookie $RABBITMQ_ERLANG_COOKIE -n rabbit@$DB_HOST start_app
+rabbitmqctl --erlang-cookie $RABBITMQ_ERLANG_COOKIE -n rabbit@$MQ_HOST start_app
 
 echo "Done."
